c7aa2ea0c0174330c630eee3e0d3e316a6611b87
zavdev <igor.zaviriukha@onix-systems.com>
2021-01-31 19:49:38 +0200

    added interactive cli menu

--- goose/cli.py
+++ goose/cli.py
@@ -1,6 +1,7 @@
 import click
 from goose.commands.goose import goose
 from goose.commands.rush import rush as goose_rush
+from goose.commands.menu import menu as goose_menu
 
 @click.group()
 def cli():
@@ -10,3 +11,7 @@ def cli():
 @click.argument('ftp_adress')
 def rush(ftp_adress):
   goose_rush(ftp_adress)
+
+@cli.command()
+def menu():
+  goose_menu()
