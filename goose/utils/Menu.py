a63ccd3c2115c1da6f13cf6526e445e6dd17a27a
zavdev <igor.zaviriukha@onix-systems.com>
2021-01-31 21:42:57 +0200

    improved menu

--- goose/utils/Menu.py
+++ goose/utils/Menu.py
@@ -3,21 +3,33 @@ import curses
 class Menu:
   def __init__(self, menu_list):
     self.menu_list = menu_list
+    self.selected_item = None
+    self.custom_text = []
+
+  def get_selected_item(self):
+    return self.selected_item
+
+  def place_custom_text(self, stdscr, h, w):
+    for i, row in enumerate(self.custom_text):
+      x = 2
+      y = 2 - len(self.custom_text) // 2 + i
+      stdscr.addstr(y, x, row)
 
   def draw_menu(self, stdscr, selected_row_idx):
     stdscr.clear()
     h, w = stdscr.getmaxyx()
+    self.place_custom_text(stdscr, h, w)
 
     for i, row in enumerate(self.menu_list):
-      x = w // 2 - len(row) // 2
+      # x = w // 2 - len(row) // 2
       y = h // 2 - len(self.menu_list) // 2 + i
 
       if i == selected_row_idx:
         stdscr.attron(curses.color_pair(1))
-        stdscr.addstr(y, x, row)
+        stdscr.addstr(y, 2, row)
         stdscr.attroff(curses.color_pair(1))
       else:
-        stdscr.addstr(y, x, row)
+        stdscr.addstr(y, 2, row)
 
     stdscr.refresh()
 
@@ -37,14 +49,18 @@ class Menu:
       elif key == curses.KEY_DOWN and current_row_idx < len(self.menu_list) - 1:
         current_row_idx += 1
       elif key == curses.KEY_ENTER or key in [10, 13]:
-        if current_row_idx == len(self.menu_list) - 1:
-          break
-        stdscr.addstr(0, 0, "You pressed {}".format(self.menu_list[current_row_idx]))
-        stdscr.refresh()
-        stdscr.getch()
+        # if current_row_idx == len(self.menu_list) - 1:
+        #   break
+        self.selected_item = current_row_idx
+        # stdscr.addstr(0, 0, "You pressed {}".format(self.menu_list[current_row_idx]))
+        # stdscr.refresh()
+        # stdscr.getch()
+        break
+
 
       self.draw_menu(stdscr, current_row_idx)
       stdscr.refresh()
 
-  def print(self):
+  def print_with(self, custom_text=[]):
+    self.custom_text = custom_text
     curses.wrapper(self.menu)
