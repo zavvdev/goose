f8c8bb7d3a40aec0c1ead3b7e7a042ff6974cb57
zavdev <igor.zaviriukha@onix-systems.com>
2021-02-22 22:19:27 +0200

    d

--- Forms/LoginForm.py
+++ Forms/LoginForm.py
@@ -1,4 +1,3 @@
-import npyscreen as nps
 from ftplib import FTP
 from constants.settings import testFtp
 from constants.formAccessors import formAccessors
@@ -8,43 +7,35 @@ from utils.writeToFile import writeToFile
 from utils.delFile import delFile
 from utils.readFromFile import readFromFile
 import time
+from constants.settings import testFtp
 
 ns = getNamespace(nsAccessors["Login"])
 
-class LoginButon(nps.ButtonPress):
-  def whenPressed(self):
-    host = self.parent.host.value
-    user = self.parent.username.value
-    passwd = self.parent.passwd.value
-    try:
-      nps.notify_wait(ns["messages"]["connecting"])
-      ftp = FTP(host)
-      ftp.login(user=user, passwd=passwd)
-      # data = ftp.retrlines('LIST')
-      
-      login_data = {
-        "host": host,
-        "username": user,
-        "password": passwd,
-      }
-      writeToFile(login_data, "data/login_data.json")
+class LoginForm():
+  def __init__(self):
+    self.greetingText = ns["greeting"]
+    self.titleText = ns["title"]
+    self.hostLabel = ns["labels"]["host"]
+    self.userLabel = ns["labels"]["user"]
+    self.passwdLabel = ns["labels"]["passwd"]
+    self.connectingMessage = ns["messages"]["connecting"]
+    self.errorMessage = ns["messages"]["error"]
 
-      self.parent.parentApp.change_form(formAccessors["Workspace"]) 
-    except:
-      delFile("data/login_data.json")
-      nps.notify_confirm(ns["messages"]["error"], editw=1)
+  def printGreeting(self):
+    print(self.greetingText)
 
-class ExitButton(nps.ButtonPress):
-  def whenPressed(self):
-    delFile("data/login_data.json")
-    self.parent.parentApp.switchForm(None)
+  def getLoginData(self):
+    print(self.titleText)
+    host = input(self.hostLabel)
+    username = input(self.userLabel)
+    passwd = input(self.passwdLabel)
 
-class LoginForm(nps.FormBaseNew):
-  def create(self):
-    self.add(nps.FixedText, value = ns["title"])
-    self.nextrely += 1
-    self.host = self.add(nps.TitleText, name = ns["labels"]["host"], value = testFtp["host"])
-    self.username = self.add(nps.TitleText, name = ns["labels"]["user"], value = testFtp["user"])
-    self.passwd = self.add(nps.TitleText, name = ns["labels"]["passwd"], value = testFtp["passwd"])
-    self.loginBtn = self.add(LoginButon, name = ns["buttons"]["login"])
-    self.exitBtn = self.add(ExitButton, name = ns["buttons"]["exit"])
+    loginData = {
+      "host": testFtp["host"],
+      "username": testFtp["user"],
+      "password": testFtp["passwd"]
+    }
+    
+    return loginData
+
+  
--- index.py
+++ index.py
@@ -1,13 +1,14 @@
 import npyscreen as nps
 from Forms.LoginForm import LoginForm
 from Forms.WorkspaceForm import WorkspaceForm
+from Forms.MainForm import MainForm
 from constants.formAccessors import formAccessors
 from constants.settings import appName
 from utils.writeToFile import writeToFile
 
 class App(nps.NPSAppManaged):
   def onStart(self):
-    self.addForm(formAccessors["Main"], WorkspaceForm, name = appName)
+    self.addForm(formAccessors["Main"], MainForm, name = appName)
     # self.addForm(formAccessors["Main"], LoginForm, name = appName)
 
   def change_form(self, name):
@@ -15,15 +16,10 @@ class App(nps.NPSAppManaged):
     self.resetHistory()
 
 if __name__ == '__main__':
-  host = input("Host: ")
-  user = input("Username: ")
-  passwd = input("Password: ")
+  login = LoginForm()
+  login.printGreeting()
+  loginData = login.getLoginData()
 
-  login_data = {
-    "host": host,
-    "username": user,
-    "password": passwd,
-  }
-  writeToFile(login_data, "data/login_data.json")
-  app = App()
-  app.run()
+  if writeToFile(loginData, "data/login_data.json"):  
+    app = App()
+    app.run()
