f2288979c6beb82bbe5ef76c88d2507cd212f3d6
zavdev <igor.zaviriukha@onix-systems.com>
2021-03-01 14:11:56 +0200

    fixed ls action

--- helpers/ActionVerificators/isLs.py
+++ helpers/ActionVerificators/isLs.py
@@ -1,5 +1,7 @@
 from constants.actions import Actions as Act
+from helpers.trimSpaces import trimSpaces
 
-def isLs(i):
-  return i == Act["Ls"]
+def isLs(action):
+  trimmed = trimSpaces(action)
+  return trimmed == Act["Ls"]
   pass
