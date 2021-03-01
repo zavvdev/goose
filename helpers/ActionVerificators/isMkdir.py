03a1ecc2b459217623e6e563a3720076bf81aaad
zavdev <igor.zaviriukha@onix-systems.com>
2021-03-01 15:12:36 +0200

    d

--- helpers/ActionVerificators/isMkdir.py
+++ helpers/ActionVerificators/isMkdir.py
@@ -1,8 +1,8 @@
 from constants.actions import Actions as Act
 from helpers.trimSpaces import trimSpaces
 
-def isMkdir(i):
-  trimmed = trimSpaces(i)
+def isMkdir(action):
+  trimmed = trimSpaces(action)
   s = trimmed.split(" ")
   if len(s) >= 2:
     return s[0] == Act["Mkdir"]
